32716b06b8aad6be1ea1af56a562b12cf27e6cdc
ndrd79 <ndrd7980@gmail.com>
2025-07-27 21:49:55 -0300

    Add favicon.svg to fix 404 error for favicon.ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
